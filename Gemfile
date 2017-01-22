33d73b10ba922df84f26421edc6463bc3bff2928
Christopher Walsh <walshification@gmail.com>
2017-01-22 13:57:29 -0600

    always be testing

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org' do
   gem 'sinatra', '1.4.7'
   gem 'sinatra-contrib', '1.4.7'
+  gem 'rspec', '3.5.0'
 end
---
+++ spec/web_guesser_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe "My Sinatra Application" do
+  describe '/web_guesser' do
+    it "allows accessing the web_guesser page" do
+      get '/web_guesser'
+      expect(last_response).to be_ok
+    end
+
+    it 'generates a random number between 0 and 100' do
+      get '/web_guesser'
+      expect(/^The SECRET NUMBER is [0-9]{1}[0-9]?[0]?.$/.match(last_response.body)).to be_truthy
+    end
+  end
+end
