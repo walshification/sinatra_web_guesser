93f910125cfcab1745aefabaf7c7ffff6887958b
Christopher Walsh <walshification@gmail.com>
2017-01-22 18:22:04 -0600

    includes too high, too low, and correct messages

--- spec/web_guesser_spec.rb
+++ spec/web_guesser_spec.rb
@@ -5,9 +5,24 @@ RSpec.describe "My Sinatra Application" do
       expect(last_response).to be_ok
     end
 
-    it 'generates a random number between 0 and 100' do
+    it 'prompts the user for a guess' do
       get '/web_guesser'
-      expect(/.*The SECRET NUMBER is [0-9]{1}[0-9]?[0]?\..*/.match(last_response.body)).to be_truthy
+      expect(/.*Guess a number between 0 and 100!.*/.match(last_response.body)).to be_truthy
+    end
+
+    it 'tells the user a guess is too high' do
+      get '/web_guesser?rando=42&guess=50'
+      expect(/.*Too high!.*/.match(last_response.body)).to be_truthy
+    end
+
+    it 'tells the user a guess is too low' do
+      get '/web_guesser?rando=42&guess=30'
+      expect(/.*Too low!.*/.match(last_response.body)).to be_truthy
+    end
+
+    it 'tells the user a guess is correct' do
+      get 'web_guesser?rando=42&guess=42'
+      expect(/.*You got it right!.*/.match(last_response.body)).to be_truthy
     end
   end
 end
--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-RANDOM_NUMBER = rand(101)
+RANDOM_NUMBER = Kernel.rand(101)
 
 get '/web_guesser' do
-  erb :index, :locals => { :number => RANDOM_NUMBER }
+  random_number = params[:rando].to_i || RANDOM_NUMBER
+  unless params[:guess]
+    message = 'Guess a number between 0 and 100!'
+  end
+  if params[:guess]
+    if params[:guess].to_i > random_number
+      message = 'Too high!'
+    elsif params[:guess].to_i < random_number
+      message = 'Too low!'
+    else
+      message = "You got it right!"
+    end
+  end
+  erb :index, :locals => { :message => message }
 end
