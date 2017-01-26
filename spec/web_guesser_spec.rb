f5185efdecccba366be4fa66cd049a5d73f95312
Christopher Walsh <walshification@gmail.com>
2017-01-25 22:27:41 -0600

    includes ability to be way off and refactors

--- spec/web_guesser_spec.rb
+++ spec/web_guesser_spec.rb
@@ -7,22 +7,36 @@ RSpec.describe "My Sinatra Application" do
 
     it 'prompts the user for a guess' do
       get '/web_guesser'
-      expect(/.*Guess a number between 0 and 100!.*/.match(last_response.body)).to be_truthy
-    end
-
-    it 'tells the user a guess is too high' do
-      get '/web_guesser?rando=42&guess=50'
-      expect(/.*Too high!.*/.match(last_response.body)).to be_truthy
-    end
-
-    it 'tells the user a guess is too low' do
-      get '/web_guesser?rando=42&guess=30'
-      expect(/.*Too low!.*/.match(last_response.body)).to be_truthy
+      expect(last_response.body).to include('Guess a number between 0 and 100')
     end
 
     it 'tells the user a guess is correct' do
       get 'web_guesser?rando=42&guess=42'
-      expect(/.*You got it right!.*/.match(last_response.body)).to be_truthy
+      expect(last_response.body).to include('You got it right!')
+    end
+
+    context 'off by more than ten' do
+      it 'tells the user a guess is way too high' do
+        get '/web_guesser?rando=42&guess=53'
+        expect(last_response.body).to include('Way too high!')
+      end
+
+      it 'tells the user a guess is way too low' do
+        get '/web_guesser?rando=42&guess=30'
+        expect(last_response.body).to include('Way too low!')
+      end
+    end
+
+    context 'off by less than ten' do
+      it 'tells the user a guess is too high' do
+        get '/web_guesser?rando=42&guess=50'
+        expect(last_response.body).to include('Too high!')
+      end
+
+      it 'tells the user a guess is too low' do
+        get '/web_guesser?rando=42&guess=35'
+        expect(last_response.body).to include('Too low!')
+      end
     end
   end
 end
--- web_guesser.rb
+++ web_guesser.rb
@@ -4,18 +4,21 @@ require 'sinatra/reloader'
 RANDOM_NUMBER = Kernel.rand(101)
 
 get '/web_guesser' do
-  random_number = params[:rando].to_i || RANDOM_NUMBER
-  unless params[:guess]
-    message = 'Guess a number between 0 and 100!'
-  end
-  if params[:guess]
-    if params[:guess].to_i > random_number
-      message = 'Too high!'
-    elsif params[:guess].to_i < random_number
-      message = 'Too low!'
-    else
-      message = "You got it right!"
-    end
-  end
+  message = check_guess(params[:guess])
   erb :index, :locals => { :message => message }
+end
+
+private
+
+def check_guess(guess)
+  return 'Guess a number between 0 and 100' if guess.nil?
+  # params[:rando] is set for tests
+  random_number = params[:rando].nil? ? RANDOM_NUMBER : params[:rando].to_i
+  return 'You got it right!' if guess.to_i == random_number
+
+  message = []
+  message << 'way'       if (guess.to_i - random_number).abs > 10
+  message << 'too high!' if guess.to_i > random_number
+  message << 'too low!'  if guess.to_i < random_number
+  message.join(' ').capitalize
 end
