b322d34d2702de61cbb2b2989ff3206ea063d89e
Christopher Walsh <walshification@gmail.com>
2017-01-28 15:49:38 -0600

    switch to sessions and limit guesses

--- spec/web_guesser_spec.rb
+++ spec/web_guesser_spec.rb
@@ -1,6 +1,10 @@
 RSpec.describe "My Sinatra Application" do
+  after(:each) do
+    get '/reset'
+  end
+
   describe '/web_guesser' do
-    it "allows accessing the web_guesser page" do
+    it 'allows accessing the web_guesser page' do
       get '/web_guesser'
       expect(last_response).to be_ok
     end
@@ -11,8 +15,19 @@ RSpec.describe "My Sinatra Application" do
     end
 
     it 'tells the user a guess is correct' do
-      get 'web_guesser?rando=42&guess=42'
+      get '/web_guesser?rando=42&guess=42'
       expect(last_response.body).to include('You got it right!')
+    end
+
+    it 'gives the user 5 guesses' do
+      get '/web_guesser'
+      expect(last_response.body).to include('5 guesses left!')
+    end
+
+    it 'counts down how many guesses are left' do
+      get '/web_guesser?rando=42&guess=1'
+      get '/web_guesser?rando=42&guess=1'
+      expect(last_response.body).to include('3 guesses left!')
     end
 
     context 'off by more than ten' do
@@ -36,6 +51,26 @@ RSpec.describe "My Sinatra Application" do
       it 'tells the user a guess is too low' do
         get '/web_guesser?rando=42&guess=35'
         expect(last_response.body).to include('Too low!')
+      end
+    end
+
+    context 'after 5 guesses' do
+      it 'tells players they lost' do
+        get '/web_guesser?rando=42&guess=10'
+        get '/web_guesser?rando=42&guess=10'
+        get '/web_guesser?rando=42&guess=10'
+        get '/web_guesser?rando=42&guess=10'
+        get '/web_guesser?rando=42&guess=10'
+        expect(last_response.body).to include('Nice try!')
+      end
+
+      it 'resets the guess count after' do
+        get '/web_guesser?rando=42&guess=10'
+        get '/web_guesser?rando=42&guess=10'
+        get '/web_guesser?rando=42&guess=10'
+        get '/web_guesser?rando=42&guess=10'
+        get '/web_guesser?rando=42&guess=10'
+        expect(last_response.body).to include('5 guesses left!')
       end
     end
   end
--- web_guesser.rb
+++ web_guesser.rb
@@ -1,11 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-RANDOM_NUMBER = Kernel.rand(101)
+enable :sessions
 
 get '/web_guesser' do
+  if session[:random_number].nil?
+    reset
+  end
   color, message = check_guess(params[:guess])
-  erb :index, :locals => { :message => message, :color => color }
+  erb :index, :locals => {
+    message: message,
+    color: color,
+    count: session[:guesses],
+  }
+end
+
+get '/reset' do
+  reset
 end
 
 private
@@ -13,20 +24,35 @@ private
 def check_guess(guess)
   return 'blue', 'Guess a number between 0 and 100' if guess.nil?
   # params[:rando] is set for tests
-  random_number = params[:rando].nil? ? RANDOM_NUMBER : params[:rando].to_i
+  random_number = params[:rando].nil? ? session[:random_number] : params[:rando].to_i
+  session[:guesses] -= 1
   build_message(guess.to_i, random_number)
 end
 
 def build_message(guess, random_number)
-  return 'green', 'You got it right!' if guess.to_i == random_number
+  if guess.to_i == random_number
+    reset
+    return 'green', 'You got it right! Try again with a new number!'
+  end
   message = []
-  message << 'way'       if (guess.to_i - random_number).abs > 10
-  message << 'too high!' if guess.to_i > random_number
-  message << 'too low!'  if guess.to_i < random_number
+  if session[:guesses] > 0
+    message << 'way'       if (guess.to_i - random_number).abs > 10
+    message << 'too high!' if guess.to_i > random_number
+    message << 'too low!'  if guess.to_i < random_number
+  else
+    reset
+    message << "Nice try! The number was #{random_number}. Try again with a new number!"
+  end
   color = determine_color(message)
   return color, message.join(' ').capitalize
 end
 
 def determine_color(message)
+  return 'blue' if session[:guesses] == 0
   return message.include?('way') ? '#ff0000' : '#ff6666'
+end
+
+def reset
+  session[:random_number] = Kernel.rand(101)
+  session[:guesses] = 5
 end
